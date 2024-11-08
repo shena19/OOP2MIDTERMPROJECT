93d9d27343d0cfc10a45805aafb8b9ed79167a74
Shena Tajanlangit <128554527+shena19@users.noreply.github.com>
2024-11-08 22:00:41 +0800

    my new code Employee.php
    OOP2 MIDTERM PROJECT

---
+++ Employee.php
@@ -0,0 +1,33 @@
+<?php
+
+abstract class Employee extends Person {
+    private $companyName;
+    protected $type;
+
+    public function __construct($name, $address, $age, $companyName) {
+        parent::__construct($name, $address, $age);
+        $this->companyName = $companyName;
+    }
+
+    abstract public function earnings();
+
+    public function getName() {
+        return $this->name;
+    }
+    public function getAddress() {
+        return $this->address;
+    }
+
+    public function getAge() {
+        return $this->age;
+    }
+
+    public function getCompany() {
+        return $this->companyName;
+    }
+
+    public function getType() {
+        return $this->type;
+    }
+}
+?>
