fed3013236293b4b12e5f6267a98e7b6fdec4e71
Shena Tajanlangit <128554527+shena19@users.noreply.github.com>
2024-11-08 22:02:29 +0800

    my new code HourlyEmployee.php
    OOP2 MIDTERM PROJECT

---
+++ HourlyEmployee.php
@@ -0,0 +1,26 @@
+<?php
+
+class HourlyEmployee extends Employee {
+    private $hoursWorked;
+    private $hourlyRate;
+
+    public function __construct($hoursWorked, $hourlyRate, $name, $address, $age, $companyName) {
+        parent::__construct($name, $address, $age, $companyName);
+        $this->hoursWorked = $hoursWorked;
+        $this->hourlyRate = $hourlyRate;
+        $this->type = "Hourly Employee";
+    }
+
+    public function earnings() {
+        return $this->hoursWorked > 40 ? $this->hoursWorked * ($this->hourlyRate * 1.5) : $this->hoursWorked * $this->hourlyRate;
+    }
+
+    public function getHoursWorked() {
+        return $this->hoursWorked;
+    }
+
+    public function getHourlyRate() {
+        return $this->hourlyRate;
+    }
+}
+?>
