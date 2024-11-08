2794dd905b0ecbe46f75ca3647d45ed6ca6ff179
Shena Tajanlangit <128554527+shena19@users.noreply.github.com>
2024-11-08 22:00:07 +0800

    my new  code CommissionEmployee.php
    OOP2 MIDTERM PROJECT

---
+++ CommissionEmployee.php
@@ -0,0 +1,32 @@
+<?php
+
+class CommissionEmployee extends Employee {
+    private $regularSalary;
+    private $itemSold;
+    private $commissionRate;
+
+    public function __construct($regularSalary,  $itemSold, $commissionRate, $name, $address, $age, $companyName) {
+        parent::__construct($name, $address, $age, $companyName);
+        $this->regularSalary = $regularSalary;
+        $this->itemSold = $itemSold;
+        $this->commissionRate = $commissionRate;
+        $this->type = "Commissioned Employee";
+    }
+
+    public function earnings() {
+        return $this->regularSalary + ($this->itemSold * $this->commissionRate);
+    }
+
+    public function getRegularSalary() {
+        return $this->regularSalary;
+    }
+
+    public function getItemsSold() {
+        return $this->itemSold;
+    }
+
+    public function getCommissionRate() {
+        return $this->commissionRate;
+    }
+}
+?>
